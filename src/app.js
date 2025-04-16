b353e1f5d83ebb3190d5b72e7c72b657b4d35729
charrito1216 <brancharry1216@gmail.com>
2025-04-16 18:36:04 -0500

    CRUD(READ) - HTTP.GET

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 /*importamos al framework express */
 
 import express from "express";
+import cors from  "cors"
 import categoriasRoutes from "./routers/categorias.routes.js";
 
 /*Asignamos a app toda funcionalidad para mi server web */
@@ -9,6 +10,8 @@ const app = express();
 
 /*setear un puerto ami web server */
 app.set("port",5000);
+
+app.use(cors());
 
 /*routers */
 
--- src/routers/categorias.routes.js
+++ src/routers/categorias.routes.js
@@ -1,9 +1,10 @@
-
 import {Router} from "express";
 import {methodHTPP as categoriaController} from "../controllers/categorias.controllers.js";
 /*Creamos el enrutador */
 const router = Router();
 
+/*get */
+/*Configuramos respuesta desde server metodo http get */
 router.get("/", categoriaController.getCategorias);
 
 
