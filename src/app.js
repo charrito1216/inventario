45eac70b3c5fd59e5e8ede1818cf2ee2657d232d
charrito1216 <brancharry1216@gmail.com>
2025-05-05 21:06:59 -0500

    response a endpoint raiz /

--- src/app.js
+++ src/app.js
@@ -15,6 +15,11 @@ app.use("/api/empleados",empleadoRouter);
 app.use("/api/clientes",clienteRouter);
 app.use("/api/productos",productoRouter);
 
+app.get('/',(req,res)=>{
+    res.send('REST API EN RENDER WITHOUT DATABASE ENDPOINTS')
+
+})
+
 
 export default app;
 
